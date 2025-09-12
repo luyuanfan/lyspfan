b9f8f82e8ea055227e0c35a03f7266584991c80d
luyuanfan <luyuanfan1@gmail.com>
2025-09-11 20:26:09 -0400

    Made some color changes and intro edits

--- js/footer.js
+++ js/footer.js
@@ -9,13 +9,15 @@ class FooterComponent extends HTMLElement {
       <span>[<a href="about.html">about me</a>]</span>
       <span>[<a href="trips.html">trips</a>]</span>
       <span>[<a href="stardew.html">stardew</a>]</span>
-      <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/" target="_blank">reddit</a>]</span>
       <br>
       <span onclick="Copy()" onmouseover="" style="cursor: pointer;" data-toggle="tooltip" data-placement="bottom" title="copied">[lyspfan@gmail.com]</span>
       `;
   }
 }
 
+/*
+      // <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/" target="_blank">reddit</a>]</span> 
+*/
 {/* <span>[<a href="crafts.html">crafts</a>]</span> */}
 /* <span>[email: <a href="mailto:lyspfan@gmail.com">lyspfan@gmail.com</a>]</span> */
 
