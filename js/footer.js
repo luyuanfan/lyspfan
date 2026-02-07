cc0bc0db02ebd9f8574d9f9927e0f24695ec7767
luyuanfan <luyuanfan1@gmail.com>
2026-02-07 12:39:53 -0500

    add html folder so i reindexed most things; also made stardew page better

--- js/footer.js
+++ js/footer.js
@@ -4,22 +4,16 @@ class FooterComponent extends HTMLElement {
   }
 
   connectedCallback() {
-    console.log("Custom move-handling logic here.");
     this.innerHTML = `
       <span>[<a href="about.html">about me</a>]</span>
       <span>[<a href="trips.html">trips</a>]</span>
       <span>[<a href="stardew.html">stardew</a>]</span>
+      <span>[<a href="5g.html">5g notes</a>]</span>
       <br>
       <span onclick="Copy()" onmouseover="" style="cursor: pointer;" data-toggle="tooltip" data-placement="bottom" title="copied">[lyspfan@gmail.com]</span>
       `;
   }
 }
-
-/*
-      // <span>[<a href="https://www.reddit.com/r/TVTooHigh/comments/1anljpv/wughh_guys_do_you_think_the_tv_is_too_high_eughh/" target="_blank">reddit</a>]</span> 
-*/
-{/* <span>[<a href="crafts.html">crafts</a>]</span> */}
-/* <span>[email: <a href="mailto:lyspfan@gmail.com">lyspfan@gmail.com</a>]</span> */
 
 function Copy() {
   navigator.clipboard.writeText("lyspfan@gmail.com");
---
+++ js/index-footer.js
@@ -0,0 +1,22 @@
+class FooterComponent extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <span>[<a href="html/about.html">about me</a>]</span>
+        <span>[<a href="html/trips.html">trips</a>]</span>
+        <span>[<a href="html/stardew.html">stardew</a>]</span>
+        <span>[<a href="html/5g.html">5g notes</a>]</span>
+        <br>
+        <span onclick="Copy()" onmouseover="" style="cursor: pointer;" data-toggle="tooltip" data-placement="bottom" title="copied">[lyspfan@gmail.com]</span>
+        `;
+    }
+  }
+  
+  function Copy() {
+    navigator.clipboard.writeText("lyspfan@gmail.com");
+  }
+  
+  customElements.define('indexfooter-component', FooterComponent);
